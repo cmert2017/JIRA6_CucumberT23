1198b2cca46b385c8ef2a3f71a3796d779775c02
Muha-B20 <muhammeddcakmak@outlook.com>
2020-11-08 22:46:10 -0700

    Created a new feature module muha

--- src/test/java/com/library/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/library/step_definitions/LoginStepDefinitions.java
@@ -11,7 +11,7 @@ import org.junit.Assert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginStepDefinitions_muha {
+public class LoginStepDefinitions {
 
 
     LoginPage loginPage = new LoginPage();
